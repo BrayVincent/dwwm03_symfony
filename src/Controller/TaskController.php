145c16b3391b901ef23c8d2ad7848bd04bf645ab
Vincent BRAY <develop@vincent-bray.com>
2021-03-01 13:54:50 +0100

    findAll Tasks in TaskController

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,18 @@ class TaskController extends AbstractController
     /**
      * @Route("/tasks/listing", name="task")
      */
-    public function index(): Response
+    public function taskListing(): Response
     {
+
+        // On va chercher par Doctrine le repository de nos Task
+        $repository = $this->getDoctrine()->getRepository(Task::class);
+
+        // dans ce repository nous récupérons toutes les données
+        $tasks = $repository->findAll();
+
+        // affichage des données dans le var_dumper
+        dd($tasks);
+
         return $this->render('task/index.html.twig', [
             'controller_name' => 'TaskController',
         ]);
