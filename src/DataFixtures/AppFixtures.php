29ca99ada5d513aef27bbfa99551f7ae532a58df
Vincent BRAY <develop@vincent-bray.com>
2021-03-01 13:34:00 +0100

    Create TaskController

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,36 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Task;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
+        // Création d'un nouvel objet Faker
+        $faker = Factory::create('fr_FR');
+
+        // Création entre 15 et 30 tâches aléatoirement
+        for ($t = 0; $t < mt_rand(15, 30); $t++) {
+
+            // Création d'un nouvel objet Task
+            $task = new Task;
+
+            // On nourrit l'objet Task
+            $task->setName($faker->sentence(6))
+                ->setDescription($faker->paragraph(3))
+                ->setCreatedAt(new \DateTime()) // Attention les dates sont créées en fonction du paramétrage de votre serveur
+                ->setDueAt($faker->dateTimeBetween('now', '6 months'));
+
+            // On fait persister les données
+            $manager->persist($task);
+        }
+
+        // On push le tout en BDD
         $manager->flush();
     }
 }
