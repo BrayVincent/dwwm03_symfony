3cf15c7515a57c07c7aa326ade2f1396feaaa305
Vincent BRAY <develop@vincent-bray.com>
2021-03-01 14:42:27 +0100

    Show Tasks in table

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -22,10 +22,10 @@ class TaskController extends AbstractController
         $tasks = $repository->findAll();
 
         // affichage des données dans le var_dumper
-        dd($tasks);
+        // dd($tasks);
 
         return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+            'tasks' => $tasks,
         ]);
     }
 }
