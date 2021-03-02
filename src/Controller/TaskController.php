4cddfba290debe4c90f90fe1f24a17aa07984055
Vincent BRAY <develop@vincent-bray.com>
2021-03-02 16:23:50 +0100

    Add DeleteTask Function

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -91,4 +91,22 @@ class TaskController extends AbstractController
 
         return $this->render('task/create.html.twig', ['form' => $form->createView()]);
     }
+
+
+    /**
+     * @Route("/tasks/delete/{id}", name="task_delete", requirements={"id"="\d+"})
+     *
+     * @param [type] $id
+     * @return Response
+     */
+    public function delete($id): Response
+    {
+
+        $task = $this->repository->findOneById($id);
+
+        $this->manager->remove($task);
+        $this->manager->flush();
+
+        return $this->redirectToRoute('tasks_listing');
+    }
 }
